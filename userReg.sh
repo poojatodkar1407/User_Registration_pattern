7987be403aeea8409e6b48f8da201e569a2c8b11
pooja todkar <poojatodkar124@gmail.com>
2019-11-21 11:44:24 +0530

    mobile number validation

--- userReg.sh
+++ userReg.sh
@@ -5,9 +5,10 @@ echo "User Registration form"
 read -p "Enter user name: " firstName
 read -p "Enter Last name: " lastName
 read -p "Enter Email Id of user name: " emailId
+read -p "Enter Mobile number: " mobileNumber
 namePattern="^[A-Z]{1}[a-z]{2,}$"
 emailPattern="^[a-zA-Z0-9][-._+a-zA-Z0-9]*[@]{1}[a-z0-9A-Z]*[.]{1}[a-z]{2,3}[.]{0,1}([a-z]{2,3}){0,1}$"
-
+mobilePattern="^[0-9]{2}[[:space:]][0-9]{10}$"
 function firstNameValidation(){
 
 	if [[ $firstName =~ $namePattern ]]
@@ -38,6 +39,18 @@ function emailIdValidation(){
 	fi
 }
 
+function mobileNumberValidation(){
+
+	if [[ $mobileNumber =~ $mobilePattern ]]
+        then
+                echo "Valid $mobileNumber"
+        else
+                echo "Invalid $mobileNumber"
+        fi
+
+}
+
 firstNameValidation
 lastNameValidation
 emailIdValidation
+mobileNumberValidation
