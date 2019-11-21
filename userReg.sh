f6624d3b42d9126c10fc3933219227f07539ab43
pooja todkar <poojatodkar124@gmail.com>
2019-11-21 18:05:21 +0530

    numeric validation done

--- userReg.sh
+++ userReg.sh
@@ -13,7 +13,7 @@ emailPattern="^[a-zA-Z0-9][-._+a-zA-Z0-9]*[@]{1}[a-z0-9A-Z]*[.]{1}[a-z]{2,3}[.]{
 mobilePattern="^[0-9]{2}[[:space:]][0-9]{10}$"
 passwordPattern="^[a-zA-Z]{8}"
 passwordPattern1="([A-Z]+[a-z]*)+|([a-z]*[A-Z]+)+|([a-z]*[A-Z]+[a-z]*)+)"
-#qw="^*[a-z]*[A-Z]{8,}$"
+checkPassword3="^(.*+[0-9]+.*[A-Z]+[a-zA-Z0-9])"
 function firstNameValidation(){
 
 	if [[ $firstName =~ $namePattern ]]
@@ -57,7 +57,7 @@ function mobileNumberValidation(){
 
 function passwordValidation(){
 	
-	if [[ $password =~ $emailpattern1  ]] &&  [[ ${#password} -ge 8 ]]
+	if [[ $password =~ $checkPassword3  ]] &&  [[ ${#password} -ge 8 ]]
         then
                 echo "Valid $password"
         else
@@ -67,8 +67,8 @@ function passwordValidation(){
 
 }
 
-#firstNameValidation
-#lastNameValidation
-#emailIdValidation
-#mobileNumberValidation
+firstNameValidation
+lastNameValidation
+emailIdValidation
+mobileNumberValidation
 passwordValidation
