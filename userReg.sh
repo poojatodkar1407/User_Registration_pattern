1104eaef2f726a3b56454c96f243d086a96a60bf
pooja todkar <poojatodkar124@gmail.com>
2019-11-21 11:13:27 +0530

    Refactor: for first name and email validation

--- userReg.sh
+++ userReg.sh
@@ -4,7 +4,9 @@ echo "User Registration form"
 
 read -p "Enter user name: " firstName
 read -p "Enter Last name: " lastName
-namePattern="^[A-Z]+[a-zA-Z]{2,}$"
+read -p "Enter Email Id of user name: " emailId
+namePattern="^[A-Z]{1}[a-z]{2,}$"
+emailPattern="^[a-zA-Z0-9][-._+a-zA-Z0-9]*[@]{1}[a-z0-9A-Z]*[.]{1}[a-z]{2,3}[.]{0,1}([a-z]{2,3}){0,1}$"
 
 function firstNameValidation(){
 
@@ -26,5 +28,16 @@ function lastNameValidation(){
 	fi
 }
 
+function emailIdValidation(){
+
+	if [[ $emailId =~ $emailPattern ]]
+	then
+		echo "Valid $emailId"
+	else
+		echo "Invalid $emailId"
+	fi
+}
+
 firstNameValidation
 lastNameValidation
+emailIdValidation
