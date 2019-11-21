1b4b37ce83a271ab2c4f9a93f1a4df2a964721e2
pooja todkar <poojatodkar124@gmail.com>
2019-11-21 10:38:08 +0530

    validation for last name

--- userReg.sh
+++ userReg.sh
@@ -3,12 +3,28 @@
 echo "User Registration form"
 
 read -p "Enter user name: " firstName
+read -p "Enter Last name: " lastName
+namePattern="^[A-Z]+[a-zA-Z]{2,}$"
 
-firstNamePattern="^[A-Z]+[a-zA-Z]{2,}"
+function firstNameValidation(){
 
-if [[ $firstName =~ $firstNamePattern ]]
-then
-	echo "valid $firstName"
-else
-	echo "invalid $firstName"
-fi
+	if [[ $firstName =~ $namePattern ]]
+	then
+		echo "Valid $firstName"
+	else
+		echo "Invalid $firstName"
+	fi
+}
+
+function lastNameValidation(){
+
+	if [[ $lastName =~ $namePattern ]]
+	then
+		echo "Valid $lastName"
+	else
+		echo "Invalid $lastName"
+	fi
+}
+
+firstNameValidation
+lastNameValidation
